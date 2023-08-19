a11a11500e45187129fec3e60cfb68c7f2477d15
nidhi2793 <MY_NAME@example.com>
2023-08-19 19:03:20 +0530

    details

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,15 @@
+import { Fragment } from "react";
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg"
+      title="FirstMeetup"
+      address="somecity"
+      description="1st meetup"
+    />
+  );
+}
+
+export default MeetupDetails;
